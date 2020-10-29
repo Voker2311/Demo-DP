d6c1b24a65606a7e19381fc3a1b0c02b1442d9b1
Sumith Pevekar <pevekar.sumith73@gmail.com>
2020-10-29 21:17:17 +0530

    init

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import Footer from './Footer';
-import Header from './Header';
-
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import Footer from "./Footer";
+import Header from "./Header";
+import "./Welcome.css";
 function Welcome() {
-    return (
-        <div className="welcome">
-            <div>
-                <h2>Welcome part</h2>
-            </div>
+  return (
+    <div className="container">
+      <div className="carousel">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>CRYP70N1C</h3>
+              <p>Hello!</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ"
+              alt="Third slide"
+            />
 
-        </div>
-    )
+            <Carousel.Caption>
+              <h3>CRYP70N1C</h3>
+              <p>Hello!</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>CRYP70N1C</h3>
+              <p>Hello!</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;
